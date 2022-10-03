1a2c0308650393dbb94b71740e7b7ca6f36ec2e0
jiaji.sun <jisu4854@colorado.edu>
2022-10-03 12:56:55 -0600

    add ignore file

--- server/index.js
+++ server/index.js
@@ -4,10 +4,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello from frontend!");
+});
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+  res.json({ message: "Hello from api!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
