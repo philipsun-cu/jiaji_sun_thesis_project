38a3c4189b87159067a075649d03a51e4f613901
jiaji.sun <jisu4854@colorado.edu>
2023-02-06 14:53:36 -0700

    Map is run success

--- client/src/Leaflet/basic.js
+++ client/src/Leaflet/basic.js
@@ -1,33 +1,43 @@
-import React, { useRef } from "react";
-// import Header from "components/Header";
+import React, { useRef, useState } from 'react';
+//import Header from "components/Header";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { Map, MapContainer, TileLayer } from "react-leaflet";
 import osm from "./osm-providers";
-// import { useRef } from "react";
-import "leaflet/dist/leaflet.css"
+//import { useRef } from "react";
+import "leaflet/dist/leaflet.css";
+//import ExternalInfo from "components/ExternalInfo";
 
 const BasicMap = () => {
-    // const [center, setCenter] = useState({ lat: 14.454756, lng: 75.325459 })
-    const center = { lat: 14.454756, lng: 75.325459 };
+    const [center, setCenter] = useState({ lat: 13.084622, lng: 80.248357 });
     const ZOOM_LEVEL = 9;
     const mapRef = useRef();
 
     return (
         <>
-            {/* <Header title="React Leaflet Map Example" /> */}
+            {/* <Header title="React Leaflet Map Example" />
+
+            <ExternalInfo page="leafletBasic" /> */}
+
             <div className="row">
                 <div className="col text-center">
-                    <h2> React-Leaflet - basic</h2>
-                    <p>Loading map</p>
+                    <h2>React-leaflet - Basic Openstreet Maps</h2>
+                    <p>Loading basic map using layer from maptiler</p>
                     <div className="col">
-                        {/* <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
-                            <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
-                         </MapContainer> */}
+                        <MapContainer style={mapStyle} center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+                            <TileLayer
+                                url={osm.maptiler.url}
+                                attribution={osm.maptiler.attribution}
+                            />
+                        </MapContainer>
                     </div>
                 </div>
             </div>
         </>
     );
 };
+
+const mapStyle = {
+    height: 600
+}
 
 export default BasicMap;
